e8908121f0e3df8a15459ee39b71d454845ea1fc
Mayur <mayurmallya7@gmail.com>
2021-01-14 15:59:42 +0530

    Fixed Clear All button, Project 1 complete

--- projects/1-birthday-reminder/setup/src/App.js
+++ projects/1-birthday-reminder/setup/src/App.js
@@ -8,9 +8,7 @@ function App() {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button onClick={() => console.log("You clicked on me")}>
-          Clear All
-        </button>
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
   );
