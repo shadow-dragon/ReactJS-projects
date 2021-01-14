434b2badde9a6e700c0a07ea91ecc1cdd0a19a65
Mayur <mayurmallya7@gmail.com>
2021-01-14 20:04:13 +0530

    Fetched & set tours

--- projects/2-tours/setup/src/App.js
+++ projects/2-tours/setup/src/App.js
@@ -6,6 +6,24 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if (loading) {
     return (
       <main>
