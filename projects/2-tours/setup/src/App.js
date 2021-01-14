d67cd8717c422e7c60c1e794936ad6e022923374
Mayur <mayurmallya7@gmail.com>
2021-01-14 19:53:57 +0530

    loading and tours useState created, Loading & Tours Component used

--- projects/2-tours/setup/src/App.js
+++ projects/2-tours/setup/src/App.js
@@ -1,11 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
