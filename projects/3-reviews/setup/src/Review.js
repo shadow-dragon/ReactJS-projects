55b02fe08b57ce714324c283751330d57c727477
Mayur <mayurmallya7@gmail.com>
2021-01-15 07:59:57 +0530

    onClick handlers for prev & next have been set up

--- projects/3-reviews/setup/src/Review.js
+++ projects/3-reviews/setup/src/Review.js
@@ -5,6 +5,21 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const Review = () => {
   const [index, setindex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setindex((index) => {
+      let newIndex = index + 1;
+      return newIndex;
+    });
+  };
+
+  const prevPerson = () => {
+    setindex((index) => {
+      let newIndex = index - 1;
+      return newIndex;
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -17,10 +32,10 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
