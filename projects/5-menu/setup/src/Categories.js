5550090f40b02d12fb1b4a734cf030e1b402b88a
Mayur <mayurmallya7@gmail.com>
2021-01-15 10:15:06 +0530

    Automated category filter. Project 5 completed

--- projects/5-menu/setup/src/Categories.js
+++ projects/5-menu/setup/src/Categories.js
@@ -3,12 +3,18 @@ import React from "react";
 const Categories = ({ categories, filterItems }) => {
   return (
     <div className="btn-container">
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        all
-      </button>
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        breakfast
-      </button>
+      {categories.map((category, index) => {
+        return (
+          <button
+            className="filter-btn"
+            type="button"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 };
