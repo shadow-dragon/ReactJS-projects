dc372736293bf14b76814a904f6fd5dfda83b024
Mayur <mayurmallya7@gmail.com>
2021-01-16 16:47:36 +0530

    Conitional existance of list items if list.length > 0

--- projects/10-grocery-bud/setup/src/App.js
+++ projects/10-grocery-bud/setup/src/App.js
@@ -39,10 +39,13 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List items={list} />
-        <button className="clear-btn">clear items</button>
-      </div>
+
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn">clear items</button>
+        </div>
+      )}
     </section>
   );
 }
