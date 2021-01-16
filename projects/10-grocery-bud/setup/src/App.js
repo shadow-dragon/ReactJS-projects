ef4b9311656d1a754d7c70e2d4b717a387be3c6b
Mayur <mayurmallya7@gmail.com>
2021-01-16 15:42:52 +0530

    Add functionality when name and not editing, pass list as items to List component

--- projects/10-grocery-bud/setup/src/App.js
+++ projects/10-grocery-bud/setup/src/App.js
@@ -10,7 +10,16 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submit works");
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edtiing
+    } else {
+      // shoe alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
   };
   return (
     <section className="section-center">
@@ -31,7 +40,7 @@ function App() {
         </div>
       </form>
       <div className="grocery-container">
-        <List />
+        <List items={list} />
         <button className="clear-btn">clear items</button>
       </div>
     </section>
