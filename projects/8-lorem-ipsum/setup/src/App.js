0e5e51d34ea852f40c7445b934e32ec22aeb61ba
Mayur <mayurmallya7@gmail.com>
2021-01-16 09:57:23 +0530

    Set up form submit. Project 8 completed

--- projects/8-lorem-ipsum/setup/src/App.js
+++ projects/8-lorem-ipsum/setup/src/App.js
@@ -6,6 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    let amount = parseInt(count);
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (amount > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
 
   return (
@@ -24,6 +32,11 @@ function App() {
           generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </section>
   );
 }
