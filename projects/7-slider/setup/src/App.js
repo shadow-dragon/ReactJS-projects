4a64cdef1e0d077c2f4c64b158056a6679f44f73
Mayur <mayurmallya7@gmail.com>
2021-01-16 08:13:40 +0530

    Automatic assignment of CSS nextSlide, activeSlide & lastSlide

--- projects/7-slider/setup/src/App.js
+++ projects/7-slider/setup/src/App.js
@@ -15,10 +15,19 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
-          // More stuff coming up
 
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
