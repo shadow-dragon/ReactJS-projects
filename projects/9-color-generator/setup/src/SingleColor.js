5c31073e667e451f32a7ed9ca54e87af5b358e74
Mayur <mayurmallya7@gmail.com>
2021-01-16 14:40:08 +0530

    Can copy hex to clipboard. Need to make alert go away after 3 seconds

--- projects/9-color-generator/setup/src/SingleColor.js
+++ projects/9-color-generator/setup/src/SingleColor.js
@@ -9,9 +9,14 @@ const SingleColor = ({ rgb, weight, index }) => {
     <article
       className={`color ${index > 10 && "color-light"}`}
       style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">{hex}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
     </article>
   );
 };
