accf9f4d33676acce0a6614fe0191548cd27aee2
Mayur <mayurmallya7@gmail.com>
2021-01-16 08:20:20 +0530

    Manual Slider complete

--- projects/7-slider/setup/src/App.js
+++ projects/7-slider/setup/src/App.js
@@ -5,6 +5,16 @@ import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
   return (
     <section className="section">
       <div className="title">
@@ -36,10 +46,10 @@ function App() {
             </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
