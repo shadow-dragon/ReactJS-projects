ac98059787e249dad6e620f9b903588174b3c9bb
Mayur <mayurmallya7@gmail.com>
2021-01-16 13:25:53 +0530

    Pass color info to SingleColor Component

--- projects/9-color-generator/setup/src/App.js
+++ projects/9-color-generator/setup/src/App.js
@@ -38,7 +38,9 @@ function App() {
         </form>
       </section>
       <section className="colors">
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
