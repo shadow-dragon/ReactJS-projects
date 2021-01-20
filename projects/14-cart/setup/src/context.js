e67a9cb505d3001a51fc6041f198f00da341f1b2
Mayur <mayurmallya7@gmail.com>
2021-01-20 10:37:35 +0530

    useReducer but reducer not set

--- projects/14-cart/setup/src/context.js
+++ projects/14-cart/setup/src/context.js
@@ -5,8 +5,15 @@ import reducer from "./reducer";
 const url = "https://course-api.com/react-useReducer-cart-project";
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+  const [state, dispatch] = useReducer(initialState);
 
   return (
     <AppContext.Provider
